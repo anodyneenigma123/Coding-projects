42b26445cdc21021b3984179a35a65b9d68a4911
Bernard Kwarteng <kwartengbernard25@gmail.com>
2026-09-03 07:28:19 -0700

    Add ATMWelcomeScreen

---
+++ Java Projects/src/ATM_Welcome_Screen.java
@@ -0,0 +1,29 @@
+/**
+ * ATM Welcome Screen Project
+ * Beginner Java Project #1
+ * Author: Bernard Nana Kwarteng
+ */
+
+public class ATM_Welcome_Screen {
+
+    public static void main(String[] args) {
+
+        // Display the top border
+        System.out.println("=========================");
+
+        // Display bank name
+        System.out.println("     KWARTENG BANK");
+
+        // Display separator
+        System.out.println("=========================");
+
+        // Welcome the user
+        System.out.println("Welcome Bernard!");
+
+        // Display application version
+        System.out.println("Java ATM System v1.0");
+
+        // Display bottom border
+        System.out.println("=========================");
+    }
+}
